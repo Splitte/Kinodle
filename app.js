669d8f9f515fa0b4b2599ed714d2c706f92403cc
Splitte <120155142+Splitte@users.noreply.github.com>
2022-12-16 23:20:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let keys = {
 let guesses = [];
 let currentGuess = [];
 
-const SecretWord = 'delfa'
+const SecretWord = 'pepaz'
 const NumberOfGuesses = 6;
 const Correct = 'correct';
 const Found = 'found';
